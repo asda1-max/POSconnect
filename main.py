27cc3a5e69bf186361c8b75cd4cd42c2f01d5fbf
azeroth <rakhathrd1314@gmail.com>
2025-12-13 15:22:20 +0700

    creating gui, testing button`

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import customtkinter as gui
+from utils import printManager as PM
 
-app = gui.CTk()
-app.geometry("600x500")
-app.title("600x500")
+class appGui:
 
-app.mainloop()
+    def __init__(self):
+        self.canvas = gui.CTk()
+        self.canvas.geometry("500x500")
+        self.canvas.title("konz")
+
+        self.text1 = gui.CTkLabel(self.canvas, text="Print a test page", font=("arial", 40, "bold"))
+        self.text1.pack(pady =(30,0))
+        self.button1 = gui.CTkButton(self.canvas, command=PM.printThis,text="test print")
+        self.button1.pack(pady = (30,0))
+    
+    def startGUI(self):
+        self.canvas.mainloop()
+
+
+app = appGui()
+app.startGUI()
