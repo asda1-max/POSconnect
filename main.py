28ad54c6de6cc0a82e1c945f2fd10480e6bf84b9
azeroth <rakhathrd1314@gmail.com>
2025-12-14 14:54:37 +0700

    gui update

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import customtkinter as gui
 from utils import printManager as PM
 
-class appGui:
+
+    
+class appGui(gui.CTk):
+    """
+    Gui Manager Class
+    """
 
     def __init__(self):
-        self.canvas = gui.CTk()
-        self.canvas.geometry("500x500")
-        self.canvas.title("konz")
+        super().__init__()
+        self.title = gui.CTkLabel(self, text="Print a test page", font=("arial", 40, "bold"))
+        self.textbox1  = gui.CTkTextbox(self, height=50)
+        self.textbox2  = gui.CTkTextbox(self)
+        self.button1 = gui.CTkButton(self, command=self.openprint,text="test print")
 
-        self.text1 = gui.CTkLabel(self.canvas, text="Print a test page", font=("arial", 40, "bold"))
-        self.text1.pack(pady =(30,0))
-        self.button1 = gui.CTkButton(self.canvas, command=PM.printThis,text="test print")
+        self.title.pack(pady =(30,0))
+        self.textbox1.pack(pady =(30,0))
+        self.textbox2.pack(pady =(30,0))
         self.button1.pack(pady = (30,0))
     
     def startGUI(self):
-        self.canvas.mainloop()
+        """
+        Start the GUI
+        """
+        self.mainloop()
+
+    def openprint(self):
+        """
+        print anything in the text box
+        """
+        self.text1 = self.textbox1.get("0.0", "end")
+        self.text2 = self.textbox2.get("0.0", "end")
+        PM.printThis(self.text1, self.text2)
+        
 
 
 app = appGui()
+app.geometry("600x500")
 app.startGUI()
--- utils/printManager.py
+++ utils/printManager.py
@@ -2,16 +2,21 @@ from win32printing import Printer
 
 print("loadded")
 
-def printThis():
+def printThis(title = "title 1",text="text"):
+    fontTitle = {
+        "height": 20,
+        "weight" : 800,
+        "underline" : 30
+        }
     font = {
         "height": 20,
         "weight" : 1,
         "underline" : 30
         }
     with Printer(linegap=1,printer_name="POS58") as printer:
-        printer.text("      title1      ", font_config=font)
-        printer.text("title2", font_config=font)
-        printer.text("title3", font_config=font)
-        printer.text("title4", font_config=font)
+        printer.text(title, font_config=fontTitle)
+        printer.text(text, font_config=font)
+        printer.text(text, font_config=font)
+        printer.text(text, font_config=font)
         printer.new_page()
         print("woi")
